0a2743f7ba205f9110cddd097082c53c94c6378e
Andres Rojas <andres.rojas.isaza@gmail.com>
2020-04-30 16:03:02 -0500

    Added changes to add two numbers

---
+++ scripts/calculator.js
@@ -0,0 +1,14 @@
+function calcular() {
+    var operador = document.getElementById("operador").value;
+    alert("Has elegido " + operador);
+
+    var primer_numero = document.getElementById("primer_numero").value;
+    var segundo_numero = document.getElementById("segundo_numero").value;
+
+    var info_resultado = document.getElementById("resultados");
+
+    if(operador == "suma") {
+        var resultado = parseInt(primer_numero) + parseInt(segundo_numero);
+        info_resultado.innerHTML = resultado;
+    }
+}
