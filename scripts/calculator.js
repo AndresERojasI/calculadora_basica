f72d54189043b63718133bb90b2408543e54e6b5
Andres Rojas <andres.rojas.isaza@gmail.com>
2020-05-04 13:13:19 -0500

    Agregué validaciones

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,9 +1,20 @@
 function calcular() {
-    var operador = document.getElementById("operador").value;
+    var operador = document.getElementById("errores").value;
+    var error_resultado = document.getElementById("resultados");
     alert("Has elegido " + operador);
 
     var primer_numero = document.getElementById("primer_numero").value;
     var segundo_numero = document.getElementById("segundo_numero").value;
+
+    if(primer_numero == null){
+        error_resultado,innerHTML = "Debes ingresar el primer número";
+        return;
+    }
+
+    if(segundo_numero == null){
+        error_resultado,innerHTML = "Debes ingresar el segundo número";
+        return;
+    }
 
     var info_resultado = document.getElementById("resultados");
 
@@ -23,6 +34,10 @@ function calcular() {
     }
 
     if(operador == "division") {
+        if(segundo_numero == 0){
+            error_resultado,innerHTML = "El segundo número no puede ser 0";
+            return;
+        }
         var resultado = parseInt(primer_numero) / parseInt(segundo_numero);
         info_resultado.innerHTML = resultado;
     }
